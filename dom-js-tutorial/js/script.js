5eaab0fe90eb80cb512279c8168cb05ccb885475
Sardorbek developer <117287653+TheSardoruz@users.noreply.github.com>
2023-01-17 21:08:33 +0500

    Add files via upload

---
+++ dom-js-tutorial/js/script.js
@@ -0,0 +1,39 @@
+let chooseBtn = document.getElementById('choose'),
+ chooseBtn2 = document.getElementById('receive'),
+ concactForm = document.getElementsByClassName('contactform_name')[0],
+ text = document.getElementsByTagName('h2')[0],
+ modal = document.querySelector('.modal'),
+ btn = document.querySelectorAll('.main_tel_title')[0],
+ closebtn = document.querySelectorAll('.close')[0],
+ message = document.getElementsByName('message')[0]
+
+ //first way
+
+function hover (){
+    text.textContent = 'Tugadi'
+}
+
+
+// second way
+// text.onmouseenter = hover
+
+
+//third way
+text.addEventListener('mouseenter', hover)
+text.addEventListener('mouseleave', function(){
+    text.textContent= 'Все включено'
+})
+
+
+chooseBtn2.addEventListener('click', function(){
+    modal.style.display = 'block'
+})
+
+closebtn.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
+concactForm.addEventListener('input', function(){
+    message.value = `меня завут ${concactForm.value}  и я хочу: `
+})
+
+
